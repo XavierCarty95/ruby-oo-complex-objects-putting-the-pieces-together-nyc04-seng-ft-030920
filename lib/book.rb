8c3894030e7adbb4506faff1bd529dc846b626c6
XavierCarty95 <xaviercarty@gmail.com>
2020-11-02 17:06:02 -0500

    commit tree a4b7c19b38f5e6bb5029d10fcf813b24e6077a31

--- lib/book.rb
+++ lib/book.rb
@@ -1,22 +1,12 @@
+class Book
+    attr_accessor :title , :author , :genre, :page_count  
 
-class Book 
-
-    attr_accessor  :author  , :page_count , :genre
-    def initialize(title)
-        @title = title 
-    end 
-
-    def title 
-        @title 
-    end 
-
-def author 
-        @author 
-    end 
-     
-      def turn_page
-        puts "Flipping the page...wow, you read fast!"
-      end
-
+ def initialize(title)
+   @title = title
+ end
+ 
+ def turn_page 
+  puts "Flipping the page...wow, you read fast!"
+ end 
 
 end 
